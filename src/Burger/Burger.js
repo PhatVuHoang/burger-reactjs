3b9e5339183c1a38a5b6f7394831656bd7af6188
vuhoangphat <phatvh.work@gmail.com>
2021-04-19 16:48:08 +0700

    update

--- src/Burger/Burger.js
+++ src/Burger/Burger.js
@@ -1,49 +1,104 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // Thư viện kết nối với Redux
-import {connect} from 'react-redux';
-import './Burger.css'
+import { connect } from "react-redux";
+import "./Burger.css";
 
 class Burger extends Component {
+  renderBreadMid = () => {
+    let { burger } = this.props;
+    return Object.entries(burger).map(([propsBurger, value], index) => {
+      let breadMid = [];
+      for (let i = 0; i < value; i++) {
+        breadMid.push(<div key={i} className={propsBurger}></div>);
+      }
 
-    renderBreadMid = () => {
-        let {burger} = this.props
-        return Object.entries(burger).map(([propsBurger, value],index) => {
+      return breadMid;
+    });
+  };
 
-            let breadMid = [];
-            for(let i = 0; i < value; i++) {
-                breadMid.push(<div key={i} className={propsBurger}></div>)
-            }
+  renderMenu = () => {
+    // lấy props từ redux về
+    let { menu, burger } = this.props;
+    return Object.entries(menu).map(([propsMenu, value], index) => {
+      return (
+        <tr key={index}>
+          <td>{propsMenu}</td>
+          <td className="text-center">
+            <button onClick={()=>{
+                this.props.addBreadMid(propsMenu, 1)
+            }} className="btn btn-success mr-2" >+</button>
+            {burger[propsMenu]}
+            <button onClick={()=>{
+                this.props.addBreadMid(propsMenu, -1)
+            }} className="btn btn-danger ml-2">-</button>
+          </td>
+          <td>${value}</td>
+          <td>${value * burger[propsMenu]}</td>
+        </tr>
+      );
+    });
+  };
 
-            return breadMid
-        })
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <h3 className="display-4 text-success font-weight-bold">Burger Store</h3>
-                <div className="row">
-                    <div className="col-7">
-                        <h3 className="text-center text-danger">Bánh burger của bạn</h3>
-                        <div className="breadTop"></div>
-                        {this.renderBreadMid()}
-                        <div className="breadBottom"></div>
-                    </div>
-                    <div className="col-5">
-
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <h3 className="display-4 text-success font-weight-bold mb-5 text-center">
+          Burger Store
+        </h3>
+        <div className="row">
+          <div className="col-7">
+            <h3 className="text-center text-danger">Your burger</h3>
+            <div className="breadTop"></div>
+            {this.renderBreadMid()}
+            <div className="breadBottom"></div>
+          </div>
+          <div className="col-5">
+            <h3 className="text-center text-success">Choose quantity food</h3>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Food</th>
+                  <th></th>
+                  <th>Unit price</th>
+                  <th>Price</th>
+                </tr>
+                {this.renderMenu()}
+              </thead>
+              <tfoot>
+                  <tr>
+                      <td colSpan="2"></td>
+                      <td>Total: </td>
+                      <td>${this.props.total}</td>
+                  </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    burger: state.BurgerReducer.burger,
+    menu: state.BurgerReducer.menu,
+    total: state.BurgerReducer.total,
+  };
+};
 
-const mapStateToProps = state => {
+const mapDispatchToProps = dispatch => {
     return {
-        burger:state.BurgerReducer.burger
+        addBreadMid: (propsBurger,amount) => {
+            //tạo action
+            const action = {
+                type:'ADD_BREADMID',
+                propsBurger,
+                amount
+            }
+            dispatch(action);
+        }
     }
 }
 
-export default connect (mapStateToProps, null)(Burger)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Burger);
--- src/Redux/reducers/BurgerReducer.js
+++ src/Redux/reducers/BurgerReducer.js
@@ -1,18 +1,32 @@
 const burgerState = {
-    burger:{
-        salad: 1,
-        cheese: 1,
-        beef: 1,
-    },
-    menu:{
-        salad: 10,
-        cheese: 15,
-        beef: 55,
-    },
-    total: 80,
-}
+  burger: {
+    salad: 1,
+    cheese: 1,
+    beef: 1,
+  },
+  menu: {
+    salad: 10,
+    cheese: 15,
+    beef: 55,
+  },
+  total: 80,
+};
 
 export const BurgerReducer = (state = burgerState, action) => {
-    
-    return {...state}
-}
+  switch (action.type) {
+    case "ADD_BREADMID": {
+      let { propsBurger, amount } = action;
+      //thay đổi số lượng
+      if (amount === -1 && state.burger[propsBurger] < 1) {
+        return { ...state };
+      }
+      let burgerUpdate = { ...state.burger };
+      burgerUpdate[propsBurger] += amount;
+      state.burger = burgerUpdate;
+      //tính tổng tiền
+      state.total += amount * state.menu[propsBurger];
+      return { ...state };
+    }
+  }
+  return { ...state };
+};
